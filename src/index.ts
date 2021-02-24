5a8f5b7dcc0be991132d8bd9ff848e78691bf2d6
Yoon Young Hoon <wodlxosxos73@gmail.com>
2021-02-24 16:38:34 +0900

    TS-base practice
    setting, interface, object, ts-watch

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+interface Human {
+    name:string,
+    age: number,
+    gender: string
+}
+
+const person = {
+    name: "younghoon",
+    age: 23,
+    gender: "male"
+}
+
+
+const sayHi = (person: Human): void =>{
+    console.log(
+        `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+    )
+}
+
+sayHi(person)
+
+export {};
